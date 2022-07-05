80ac1fadde3377adb5b02aa886cc5599a59c0312
manue <manue@LAPTOP-TKTP7A3B>
2022-07-05 18:35:26 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio1DeManuelPenialva/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ejercicio1DeManuelPenialva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double CantidadDePesos, Dolares, Euros;
+            Console.WriteLine(" Ingrese La Cantidad De Pesos ");
+            CantidadDePesos = double.Parse(Console.ReadLine());
+            Dolares = (CantidadDePesos * 230);
+            Console.WriteLine($"La Cantidad De Dolares Que Puede Comprar Con Los Pesos Ingresados Es De {Dolares} Dolares");
+            Euros = Dolares * 1.17;
+            Console.WriteLine($"El Equivalente En Euros Es {Euros} Euros");
+
+        }
+    }
+}
